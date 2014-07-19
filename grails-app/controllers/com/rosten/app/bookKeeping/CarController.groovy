7d33760f63ec52371da2641759f2753ddc3b941f
chinarosten <luhangyu2000@163.com>
2014-07-19 17:19:25 +0800

    完善删除/保存功能

--- grails-app/controllers/com/rosten/app/bookKeeping/CarController.groovy
+++ grails-app/controllers/com/rosten/app/bookKeeping/CarController.groovy
@@ -5,12 +5,69 @@ import com.rosten.app.util.FieldAcl
 import com.rosten.app.system.Company
 import com.rosten.app.system.User
 import com.rosten.app.util.Util
+import com.rosten.app.system.Depart
 
 class CarController {
 
 	def carService
 	def springSecurityService
 	
+	def carRegisterSave ={
+		def json=[:]
+		def company = Company.get(params.companyId)
+		
+		//车辆基本信息---------------------------
+		def carBaseInfor = new CarBaseInfor()
+		if(params.carBaseInforId && !"".equals(params.carBaseInforId)){
+			carBaseInfor = CarBaseInfor.get(params.carBaseInforId)
+		}else{
+			carBaseInfor.company = company
+		}
+		carBaseInfor.properties = params
+		carBaseInfor.clearErrors()
+		//----------------------------------------
+		
+		//车辆登记信息保存-------------------------------
+		def carRegister = new CarRegister()
+		if(params.id && !"".equals(params.id)){
+			carRegister = CarRegister.get(params.id)
+		}else{
+			carRegister.company = company
+			carRegister.carBaseInfor = carBaseInfor
+		}
+		carRegister.properties = params
+		carRegister.clearErrors()
+		
+		//特殊字段信息处理
+		carRegister.buyDate = Util.convertToTimestamp(params.buyDate)
+		carRegister.userDepart = Depart.get(params.departId)
+		
+		if(carRegister.save(flush:true)){
+			json["result"] = "true"
+		}else{
+			carRegister.errors.each{
+				println it
+			}
+			json["result"] = "false"
+		}
+		render json as JSON
+	}
+	def carRegisterDelete ={
+		def ids = params.id.split(",")
+		def json
+		try{
+			ids.each{
+				def carRegister = CarRegister.get(it)
+				if(carRegister){
+					carRegister.delete(flush: true)
+				}
+			}
+			json = [result:'true']
+		}catch(Exception e){
+			json = [result:'error']
+		}
+		render json as JSON
+	}
 	def carRegisterGrid ={
 		def json=[:]
 		def company = Company.get(params.companyId)

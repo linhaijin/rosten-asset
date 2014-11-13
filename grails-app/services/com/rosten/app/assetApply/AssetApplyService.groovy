ab12c0ce437200ea1c5cae339696fbe23e92d71d
chinarosten <luhangyu2000@163.com>
2014-11-13 15:22:53 +0800

    增加搜索功能

--- grails-app/services/com/rosten/app/assetApply/AssetApplyService.groovy
+++ grails-app/services/com/rosten/app/assetApply/AssetApplyService.groovy
@@ -6,16 +6,16 @@ class AssetApplyService {
 
 	def springSecurityService
 	
-	def getMineApplyDataStore ={params->
+	def getMineApplyDataStore ={params,searchArgs->
 		Integer offset = (params.offset)?params.offset.toInteger():0
 		Integer max = (params.max)?params.max.toInteger():15
-		def propertyList = getAllMineApply(offset,max,params.company)
+		def propertyList = getAllMineApply(offset,max,params.company,searchArgs)
 
 		def gridUtil = new GridUtil()
 		return gridUtil.buildDataList("id","title",propertyList,offset)
 	}
 	
-	def getAllMineApply ={offset,max,company->
+	def getAllMineApply ={offset,max,company,searchArgs->
 		def user = springSecurityService.getCurrentUser()
 		def c = ApplyNotes.createCriteria()
 		def pa=[max:max,offset:offset]
@@ -25,11 +25,19 @@ class AssetApplyService {
 				eq("currentUser",user)
 				eq("applyUser",user)
 			}
+			searchArgs.each{k,v->
+				if(k.equals("userCategory")){
+					eq(k,v)
+				}else{
+					like(k,"%" + v + "%")
+				}
+				
+			}
 		}
 		return c.list(pa,query)
 	}
 	
-	def getMineApplyCount ={company->
+	def getMineApplyCount ={company,searchArgs->
 		def user = springSecurityService.getCurrentUser()
 		def c = ApplyNotes.createCriteria()
 		def query = {
@@ -37,6 +45,14 @@ class AssetApplyService {
 			or{
 				eq("currentUser",user)
 				eq("applyUser",user)
+			}
+			searchArgs.each{k,v->
+				if(k.equals("userCategory")){
+					eq(k,v)
+				}else{
+					like(k,"%" + v + "%")
+				}
+				
 			}
 		}
 		return c.count(query)
